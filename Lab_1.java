7381b533c4308d869b12db0f136591b6c76a0a5a
Wilson <130201427+AwakenWilson3961@users.noreply.github.com>
2024-09-27 18:18:24 -0400

    Add files via upload

---
+++ Lab_1.java
@@ -0,0 +1,50 @@
+public class Lab_1 {
+	public static void main(String[] args) {
+		// This will print out my name Twice in two separate lines
+		System.out.println("Wilson");
+		System.out.println("Wilson");
+		
+		// This will print out my name Twice in the same line 
+		
+		System.out.println("Wilson" + " ");
+		System.out.println("Wilson");
+		
+		// This will print Welcome to Java three times
+		
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		
+		// This will calculate the PEMDAS 
+		
+		double result = (9.5 * 4.5 - 2.5 *3) / (45.5-3.5);
+		
+		System.out.println(result);
+		
+		// This will summarize the following numbers 1,2,3,4,5,6,7,8,9
+		
+		System.out.println(1+2+3+4+5+6+7+8+9);
+		
+		// This will calculate the factorial of the number 7
+		
+		System.out.println(7 * 6 * 5 * 4 * 3 * 2 * 1);
+		
+		// This will print out he following stored variables 
+		
+		String Name = "John Doe";
+		int Age = 20;
+		double Height = 5.11;
+		
+		System.out.println(Name + " " + Age + " " +  Height);
+		
+		// This will calculate the area of a circle with a radius of 5.5 
+		
+		double radius = 5.5;
+		double pi = 3.14159;
+		
+		System.out.println(radius * radius * pi);
+		
+		
+	}
+
+}
